cb20edb6ffb24f2ad14d9de9a7acaeadf30657dd
Bernd Brägelmann <bernd@braegelmann.net>
2013-05-26 22:36:46 +0200

    optimized job fixtures, added unit test for jobs

--- test/unit/job_test.rb
+++ test/unit/job_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class JobTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "job attributes must not be empty" do
+    job = Job.new
+    assert job.invalid?
+    assert job.errors[:title].any?
+    assert job.errors[:description].any?
+    assert job.errors[:price].any?
+    assert job.errors[:image_url].any?
+  end
+
 end
