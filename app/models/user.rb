b062d6c480bf65aeff1b75273bd0ce8da786519b
Bernd Brägelmann <bernd@braegelmann.de>
2013-05-27 11:11:38 +0200

    fixed functional job tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation
 
   validates_format_of :email, :with => /@/
+  validates :email, :uniqueness => true
+
   has_secure_password
   validates_presence_of :password, :on => :create
 
