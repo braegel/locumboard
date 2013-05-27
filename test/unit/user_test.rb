b1ab9a1149c28ade1a89698e0c21f73788ed1d2b
Bernd Brägelmann <bernd@braegelmann.de>
2013-05-27 11:16:54 +0200

    some user unit tests

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "user attributes must not be empty" do
+    user = User.new
+    assert user.invalid?
+    assert user.errors[:email].any?
+    assert user.errors[:password_digest].any?
+    assert user.errors[:password].any?
+  end
+
+  test "user fixture :one is valid" do
+    user = users(:one)
+    assert user.valid?
+  end
+
 end
