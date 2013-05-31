0957e81e5121f25bd711df6b185e6b7d537964da
Bernd Brägelmann <bernd@braegelmann.de>
2013-05-31 17:39:58 +0200

    mail sending is working

--- config/environment.rb
+++ config/environment.rb
@@ -5,9 +5,7 @@ require File.expand_path('../application', __FILE__)
 Locumboard::Application.initialize!
 
 Locumboard::Application.configure do
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.raise_delivery_errors = true
 
-  config.action_mailer.smtp_settings = {
-    address: "localhost",
-  }
 end
