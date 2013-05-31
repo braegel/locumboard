a9569e62a584732cbca1ba139f30e260e1395e66
Bernd Brägelmann <bernd@braegelmann.de>
2013-05-31 16:51:55 +0200

    configuring smtp

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,11 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Locumboard::Application.initialize!
+
+Locumboard::Application.configure do
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: "localhost",
+  }
+end
