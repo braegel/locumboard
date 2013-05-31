3f1c7beefd19f2d951ac74830d9de6eb3b6e506e
Bernd Brägelmann <bernd@braegelmann.de>
2013-05-31 19:16:13 +0200

    validations + deleting scaffold controller

--- app/models/emailvalidation.rb
+++ app/models/emailvalidation.rb
@@ -1,3 +1,7 @@
 class Emailvalidation < ActiveRecord::Base
   attr_accessible :key, :user_id
+  belongs_to :user
+  validates :user_id, :uniqueness => true
+  validates :user_id, :key, :presence => true
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130527073636) do
+ActiveRecord::Schema.define(:version => 20130531170620) do
+
+  create_table "emailvalidations", :force => true do |t|
+    t.integer  "user_id"
+    t.string   "key"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "jobs", :force => true do |t|
     t.integer  "provider"
