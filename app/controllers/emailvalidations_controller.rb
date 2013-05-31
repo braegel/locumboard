e82cdd9db465c76d4e9c8dc0bd046cb79bc8b197
Bernd Brägelmann <bernd@braegelmann.net>
2013-05-31 22:56:30 +0200

    emailvalidation get created and saved upon user creation

--- app/controllers/emailvalidations_controller.rb
+++ app/controllers/emailvalidations_controller.rb
@@ -1,20 +1,6 @@
 class EmailvalidationsController < ApplicationController
   skip_before_filter :authorize
 
-  def new
-    @emailvalidation = Emailvalidation.new
-    @emailvalidation.key = (0...63).map{ ('a'..'z').to_a[rand(26)] }.join
-  end
-
-  def create(userid)
-    @emailvalidation = Emailvalidation.new(user_id: userid)
-    old = Emailvalidation.where("user_id = #{userid}")
-    old.destroy
-    if @emailvalidation.safe
-      UserMailer.welcome_email(user).deliver
-    end
-  end
-
   def validate
     if Emailvalidations.where("key = #{params[:key]}").exists?
       emailvalidation = Emailvalidations.where("key = #{params[:key]}");
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,10 +8,27 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
-      Emailvalidation.create
-      redirect_to root_url, :notice => "User created. A validation email has been sent to #{@user.email}!"
+
+      emailvalidation=Emailvalidation.new
+      @emailvalidation = Emailvalidation.new
+      @emailvalidation.user_id = @user.id
+      @emailvalidation.key = (0...63).map{ ('a'..'z').to_a[rand(26)] }.join
+
+      logger.debug "Emailvalidation created: #{@emailvalidation.inspect}"
+
+      while Emailvalidation.where("user_id = #{@user.id}").exists? do
+        Emailvalidation.where("user_id = #{@user.id}").first.destroy
+      end
+
+      if @emailvalidation.save
+        UserMailer.welcome_email(@user).deliver
+        redirect_to root_url, :notice => "User created. A validation email has been sent to #{@user.email}!"
+      else
+        render "new", :notice => "Validation key could not be created"
+      end
     else
-      render "new"
+      render "new", :notice => "User could not be saved"
     end
+
   end
 end
