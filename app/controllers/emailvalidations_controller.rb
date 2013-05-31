ef9c05c100b70787f167452742c2a003fe96532e
Bernd Brägelmann <bernd@braegelmann.de>
2013-05-31 21:04:13 +0200

    preparing emailvalidations validate controller

--- app/controllers/emailvalidations_controller.rb
+++ app/controllers/emailvalidations_controller.rb
@@ -2,8 +2,13 @@ class EmailvalidationsController < ApplicationController
   skip_before_filter :authorize
 
   def validate
-    puts :key
-    redirect_to root_url , :notice => "Account validated!"
+    if Emailvalidations.where("key = #{params[:key]}").exists?
+      emailvalidation = Emailvalidations.where("key = #{params[:key]}");
+#todo: validate in user db
+      redirect_to root_url , :notice => "Account validated!"
+    else
+      redirect_to root_url , :notice => "Key invalid!"
+    end
   end
 
 end
